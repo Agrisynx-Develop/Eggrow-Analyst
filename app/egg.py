57f24382609cd25ff8da9275e7a0c7921fcd152e
Teguh  Guntoro <teguhguntoro28@gmail.com>
2026-04-22 21:41:32 +0700

    Update egg.py

--- app/egg.py
+++ app/egg.py
@@ -840,7 +840,7 @@ elif menu == "Kesehatan":
                 st.error("AI tidak tersedia")
 
     @st.cache_resource
-
+    import os
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     model_path = os.path.join(BASE_DIR, "..", "model", "eggrow_vision_model.h5")
 
