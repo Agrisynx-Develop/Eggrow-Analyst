fd727d0f64341e7072db775eba6adbe7f8885289
Teguh  Guntoro <teguhguntoro28@gmail.com>
2026-04-23 05:15:31 +0700

    Delete convert_model.py

--- convert_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-# load model lama
-model = tf.keras.models.load_model("model/eggrow_vision_model.keras", compile=False)
-
-# save ke H5
-model.save("model/model_fix.h5")
-
-print("✅ Model berhasil di-convert")
