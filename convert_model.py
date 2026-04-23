45ffab11a6d2b01dfd724b5041c6cf53776796e6
Teguh  Guntoro <teguhguntoro28@gmail.com>
2026-04-23 11:56:23 +0700

    Update convert_model.py

--- convert_model.py
+++ convert_model.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
 # load model lama
-model = tf.keras.models.load_model("model/eggrow_vision_model.h5", compile=False)
+model = tf.keras.models.load_model("model/eggrow_vision_keras", compile=False)
 
 # rebuild model (INIT ulang graph)
 model = tf.keras.models.clone_model(model)
 model.set_weights(model.get_weights())
 
 # save ulang bersih
-model.save("model/clean_model.keras")
+model.save("model/clean_model.h5")
